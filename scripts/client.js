1db335eefaaa83919c90b1c6affbf41b558bb631
Joel Burdorf <joelburdorf@Joels-MacBook-Pro.local>
2020-01-31 10:46:52 -0600

    loop append

--- scripts/client.js
+++ scripts/client.js
@@ -2,7 +2,35 @@ console.log('IN JS')
 
 $(document).ready(onReady);
 
+//songs app
+let songs = [];
+
 function onReady() {
     console.log('in onReady');
-    
+    $('#updateSongsButton').on('click', displaySongs);
+
 } //end onReady
+
+/// - test song
+
+function displaySongs () {
+    console.log('in displaySongs');
+
+    // target an output element by ID
+    let el = $('#songsOut');
+    //empty out element
+    el.empty();
+    //loop through songs array
+    for (let i=0; i<songs.length; i++){
+        // append each song to the DOM
+        el.append(`<li>${songs[i].artist} ${songs[i].title} ${songs[i].album} ${songs[i].year}</li>`)
+        } // end for
+    
+} //end displaySongs
+
+songs.push( {
+    artist: 'Red Hot Chile Peppers',
+    title: 'Under the Bridge',
+    album: 'Blood Sugar Sex Magik',
+    year: 1991
+});
